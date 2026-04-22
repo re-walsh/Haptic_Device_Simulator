4d40133b8856915edc41d93c027781a53736b809
Robert Emil Walsh <83039983+re-walsh@users.noreply.github.com>
2026-04-21 23:54:42 -0700

    Update vis_launch.py

--- src/Simulation/launch/vis_launch.py
+++ src/Simulation/launch/vis_launch.py
@@ -16,7 +16,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     pkg_share = Path(get_package_share_directory('haptic_sim'))
     
-    rviz_config = pkg_share / 'Visualization' / 'view_haptic.rviz'
+    rviz_config = pkg_share / 'visualization' / 'view_haptic.rviz'
     urdf_template = pkg_share / 'urdf' / 'hapticdevice_URDF.urdf'
     with open(urdf_template, 'r') as infp:
         robot_description_config = infp.read()
